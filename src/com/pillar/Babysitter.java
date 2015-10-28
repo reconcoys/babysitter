520d7cf91c333d92dcbdc3e666d241138749ae6b
connorsweeney <cs293407@gmail.com>
2015-10-27 23:28:28 -0400

    Refactor

--- src/com/pillar/Babysitter.java
+++ src/com/pillar/Babysitter.java
@@ -5,23 +5,19 @@ import org.joda.time.Duration;
 
 public class Babysitter {
 
+    private static final long START_TO_BED_PAY = 12;
+    private static final long BED_TO_MIDNIGHT_PAY = 8;
+    private static final long MIDNIGHT_TO_END_PAY = 16;
+
     public long calculatePay(DateTime start, DateTime end, DateTime bedTime) throws Exception {
 
-        if (start.getHourOfDay() < 17) {
-            throw new Exception("Babysitter starts no earlier than 5pm");
-        }
-        if (end.getHourOfDay() > 4 && end.getDayOfMonth() > start.getDayOfMonth()) {
-            throw new Exception("Babysitter leaves no later than 4am");
-        }
-        if (bedTime.getDayOfMonth() != start.getDayOfMonth()) {
-            throw new Exception("Bedtime must be before midnight");
-        }
+        validateInput(start, end, bedTime);
 
         DateTime midnight = (new DateTime(start.getYear(), start.getMonthOfYear(), start.getDayOfMonth(), 0, 0)).plusDays(1);
-        Duration beforeBedDuration = end.isAfter(bedTime) ? new Duration(start, bedTime) : new Duration(start, end);
         Duration bedToMidnightDuration = new Duration(0);
         Duration midnightToEndDuration = new Duration(0);
 
+        Duration beforeBedDuration = end.isAfter(bedTime) ? new Duration(start, bedTime) : new Duration(start, end);
         if (end.isAfter(bedTime)) {
             if (end.isAfter(midnight)) {
                 bedToMidnightDuration = new Duration(bedTime, midnight);
@@ -33,10 +29,23 @@ public class Babysitter {
         if (end.isAfter(midnight)) {
             midnightToEndDuration = new Duration(midnight, end);
         }
-        long pay = beforeBedDuration.getStandardHours() * 12;
-        pay += bedToMidnightDuration.getStandardHours() * 8;
-        pay += midnightToEndDuration.getStandardHours() * 16;
+
+        long pay = beforeBedDuration.getStandardHours() * START_TO_BED_PAY;
+        pay += bedToMidnightDuration.getStandardHours() * BED_TO_MIDNIGHT_PAY;
+        pay += midnightToEndDuration.getStandardHours() * MIDNIGHT_TO_END_PAY;
 
         return pay;
+    }
+
+    private void validateInput(DateTime start, DateTime end, DateTime bedTime) throws Exception {
+        if (start.getHourOfDay() < 17) {
+            throw new Exception("Babysitter starts no earlier than 5pm");
+        }
+        if (end.getHourOfDay() > 4 && end.getDayOfMonth() > start.getDayOfMonth()) {
+            throw new Exception("Babysitter leaves no later than 4am");
+        }
+        if (bedTime.getDayOfMonth() != start.getDayOfMonth()) {
+            throw new Exception("Bedtime must be before midnight");
+        }
     }
 }
